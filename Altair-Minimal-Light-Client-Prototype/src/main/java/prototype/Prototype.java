6fbf9559589495823cfcd34e7c4741ade6f25dfc
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-10-03 14:35:16 -0700

    Lots of trial and error ahead into building this thing

---
+++ Altair-Minimal-Light-Client-Prototype/src/main/java/prototype/Prototype.java
@@ -0,0 +1,14 @@
+package prototype;
+
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.security.Security;
+
+
+public class Prototype {
+	
+	  public static void main(final String... args) {
+
+	  }	
+
+}
