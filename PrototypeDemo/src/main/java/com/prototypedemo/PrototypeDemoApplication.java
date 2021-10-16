795b9d1a0840b7fb4a3fefb37c1f0aeb82514bc0
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-10-16 16:36:04 -0700

    Demo and Server projects separated

---
+++ PrototypeDemo/src/main/java/com/prototypedemo/PrototypeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prototypedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeDemoApplication.class, args);
+	}
+
+}
